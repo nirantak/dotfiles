a16b0627f6348690abf254bda4d04f15952abbc0
Nirantak Raghav <me@nirantak.com>
2018-08-25 22:18:24 +0530

    Add misc bash scripts

---
+++ scripts/bkp_pd.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+echo -e "\e[32mStarting Pendrive Backup...\e[0m"
+
+rsync -CauhP --stats --delete-after --exclude "N" --dry-run /media/nirantak/NirantakPD/ ~/Downloads/PD | grep "^del"
+read -n1 -p "Do that? [y,n]: " doit
+
+case $doit in
+  y|Y) rsync -CauhP --stats --delete-after --exclude "N" /media/nirantak/NirantakPD/ ~/Downloads/PD ;;
+  n|N) echo -e "\nDid not backup" ;;
+  *) echo -e "\nInvalid Input" ;;
+esac
+
+echo -e "\e[32m*Backup Complete*\e[0m"
