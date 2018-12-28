dd1381c774b748bd2de7086a8d7fe1dd322fe0dd
Nirantak Raghav <me@nirantak.com>
2018-12-28 22:26:28 +0530

    Add hyper config, update gitconfig

---
+++ shell/.hyper.js
@@ -0,0 +1,56 @@
+// See https://hyper.is/#cfg for all currently supported options.
+
+module.exports = {
+  config: {
+    updateChannel: "stable",
+    fontSize: 14,
+    fontFamily: "'Liberation Mono for Powerline', 'Ubuntu Mono', monospace",
+    fontWeight: "normal",
+    fontWeightBold: "bold",
+    lineHeight: 1,
+    letterSpacing: 0,
+    cursorColor: "rgba(248,28,229,0.8)",
+    cursorAccentColor: "#000",
+    cursorShape: "BEAM",
+    cursorBlink: false,
+    scrollback: 10000,
+    windowSize: [1050, 520],
+    foregroundColor: "#fff",
+    backgroundColor: "#000",
+    selectionColor: "rgba(248,28,229,0.3)",
+    borderColor: "#333",
+    css: "",
+    termCSS: "",
+    showHamburgerMenu: "",
+    showWindowControls: "",
+    padding: "12px 14px",
+    colors: {
+      black: "#000000",
+      red: "#C51E14",
+      green: "#1DC121",
+      yellow: "#C7C329",
+      blue: "#0A2FC4",
+      magenta: "#C839C5",
+      cyan: "#20C5C6",
+      white: "#C7C7C7",
+      lightBlack: "#686868",
+      lightRed: "#FD6F6B",
+      lightGreen: "#67F86F",
+      lightYellow: "#FFFA72",
+      lightBlue: "#6A76FB",
+      lightMagenta: "#FD7CFC",
+      lightCyan: "#68FDFE",
+      lightWhite: "#FFFFFF"
+    },
+    shell: "",
+    shellArgs: ["--login"],
+    env: {},
+    bell: "SOUND",
+    copyOnSelect: true,
+    defaultSSHApp: true
+  },
+
+  plugins: [],
+  localPlugins: [],
+  keymaps: {}
+};
