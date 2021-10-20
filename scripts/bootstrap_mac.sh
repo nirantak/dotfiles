731ba56c45292faa5a91d3960ddfd06a206ba5ae
Nirantak Raghav <me@nirantak.com>
2021-10-20 13:13:50 +0530

    Update setup scripts

--- scripts/bootstrap_mac.sh
+++ scripts/bootstrap_mac.sh
@@ -16,15 +16,15 @@ sudo chown -R $USER:$(id -gn $USER) ~/.config
 
 echo -e "\n \x1B[32m Installing Packages \x1B[0m"
 brew install curl wget git tree htop vim tmux zsh zsh-completions bat gnu-time gnu-sed gpgme grep
-brew install python3 node go gcc rbenv nvm pandoc lynx imagemagick k9s
+brew install python3 node go gcc rbenv nvm svn pandoc lynx imagemagick k9s
 brew install autossh vnstat openvpn coreutils hping wrk mtr telnet cloudflare/cloudflare/cloudflared
 brew install gh geckodriver cloc diff-so-fancy jq fzf ripgrep pipx hyperfine
 
 echo -e "\n \x1B[32m Installing Apps \x1B[0m"
 brew install --cask rectangle iterm2 istat-menus visual-studio-code
 brew install --cask google-chrome firefox slack zoom tunnelblick
-brew install --cask docker insomnia beekeeper-studio mongodb-compass
-brew install --cask gimp spotify vlc notion
+brew install --cask spotify vlc notion microsoft-remote-desktop
+brew install --cask docker gimp insomnia beekeeper-studio mongodb-compass
 
 echo -e "\n \x1B[32m Setting up Terminal \x1B[0m"
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
@@ -43,16 +43,15 @@ curl -L "https://github.com/pyenv/pyenv-installer/raw/master/bin/pyenv-installer
 brew tap homebrew/cask-fonts
 brew tap homebrew/services
 brew install --cask font-fira-code font-source-code-pro font-hack-nerd-font font-victor-mono
+npm install -g browser-sync speed-test eslint prettier
 
 rbenv install ${RUBY_VERSION}
 rbenv global ${RUBY_VERSION}
-gem install bundler
 gem update --system
+gem install bundler rubocop rufo
 
 pyenv install ${PYTHON_VERSION}
 pyenv global ${PYTHON_VERSION}
-
-npm install -g browser-sync speed-test eslint prettier
 pip install -U pip wheel setuptools
 pip install -U black flake8 ipython
 for package in httpie youtube-dl pre-commit awscli;
