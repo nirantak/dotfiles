ed1eff0c3c42d468d6bed11d0511116ae54e4399
Nirantak Raghav <me@nirantak.com>
2023-04-04 19:14:01 +0530

    Update macOS dock apps

--- scripts/dock_reset_macos.sh
+++ scripts/dock_reset_macos.sh
@@ -18,13 +18,11 @@ echo "Setting up dock icons, mode: $MODE"
 if [[ "$MODE" == "work" ]]; then
   defaults delete com.apple.dock persistent-apps
   defaults write com.apple.dock persistent-apps -array \
-    "$(__dock_item /Applications/Google\ Chrome.app)" \
+    "$(__dock_item /Applications/Microsoft\ Edge.app)" \
     "$(__dock_item /Applications/Microsoft\ Outlook.app)" \
     "$(__dock_item /Applications/Slack.app)" \
-    "$(__dock_item /Applications/Microsoft\ Teams.app)" \
     "$(__dock_item /Applications/Visual\ Studio\ Code.app)" \
     "$(__dock_item /Applications/iTerm.app)" \
-    "$(__dock_item /Applications/Spotify.app)" \
     "$(__dock_item /Applications/Notion.app)" \
     "$(__dock_item /System/Applications/Utilities/Activity\ Monitor.app)" \
     "$(__dock_item /System/Applications/System\ Settings.app)"
