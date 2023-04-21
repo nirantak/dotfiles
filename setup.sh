77bfff9148f01dfb147739dd82a6773244b077ce
Nirantak Raghav <me@nirantak.com>
2023-04-21 14:06:26 +0530

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -3,55 +3,46 @@
 # This script creates symlinks from dotfiles dir to their respective locations
 set -euo pipefail
 IFS=$'\n\t'
-
+REPO_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 
 SAVEIFS=$IFS
 IFS=$(echo -en "\n\b")
 
-dir=~/dotfiles
-oldDir=~/dotfiles/dotfiles_old
-
 # Terminal config
-
-files=( "shell/.bash_profile" "shell/.bashrc" "shell/.zshenv" "shell/.zshrc" "shell/.inputrc" "shell/.editrc" "tools/.prettierrc" "tools/.tmux.conf" "tools/.vimrc" "git/.gitconfig" )
-
-echo "Creating $oldDir for backup of any existing dotfiles"
-mkdir -p "$oldDir/{shell,tools,git}"
-
-echo "Changing to the $dir directory"
-cd $dir
+files=( "shell/.bashrc" "shell/.zshenv" "shell/.zshrc" "shell/.inputrc" "shell/.editrc" "tools/.prettierrc" "tools/.tmux.conf" "tools/.vimrc" "git/.gitconfig" )
 
 echo "Moving existing dotfiles from ~ to $oldDir"
 for file in "${files[@]}"; do
   filename=$(basename $file)
   filedir=$(dirname $file)
 
-  if [[ -f ~/$filename ]]; then
-    mv ~/$filename $oldDir/$filedir/
+  if [[ -f ~/$filename && ! -L ~/$filename ]]; then
+    # Backup old dotfile if not a symlink
+    mv ~/$filename ~/$filename.$(date +%s).bak
   fi
   echo "Creating symlink to $filename"
-  ln -svf $dir/$file ~/$filename
+  ln -svf $REPO_DIR/$file ~/$filename
 done
 
 # VSCode config
-
 vsFiles=( "settings.json" "keybindings.json" )
+
 if [[ "$OSTYPE" == "darwin"* ]]; then
   vsPath=~/Library/Application\ Support/Code/User
 else
   vsPath=~/.config/Code/User
 fi
 
-echo "Moving existing dotfiles from $vsPath to $oldDir/vscode"
+echo "Setting up VSCode configs in $vsPath"
 mkdir -p $vsPath
-mkdir -p "$oldDir/vscode"
 
 for file in "${vsFiles[@]}"; do
-  if [[ -f $vsPath/$file ]]; then
-    mv $vsPath/$file $oldDir/vscode/
+  if [[ -f $vsPath/$file && ! -L $vsPath/$file ]]; then
+    # Backup old config if not a symlink
+    mv $vsPath/$file $vsPath/$file.$(date +%s).bak
   fi
   echo "Creating symlink to $file"
-  ln -svf $dir/vscode/$file $vsPath/$file
+  ln -svf $REPO_DIR/vscode/$file $vsPath/$file
 done
 
 # restore $IFS
