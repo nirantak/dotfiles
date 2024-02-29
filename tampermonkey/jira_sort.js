986186f12ca7541a9ac7abe4c6335f4ca3f61359
Nirantak Raghav <me@nirantak.com>
2024-02-29 12:54:04 +0530

    Update jira_sort.js

--- tampermonkey/jira_sort.js
+++ tampermonkey/jira_sort.js
@@ -9,31 +9,30 @@
 // ==/UserScript==
 
 function waitForElm(selector) {
-    return new Promise((resolve) => {
+  return new Promise((resolve) => {
+    if (document.querySelector(selector)) {
+      return resolve(document.querySelector(selector));
+    }
+
+    const observer = new MutationObserver((mutations) => {
       if (document.querySelector(selector)) {
-        return resolve(document.querySelector(selector));
-      }
-  
-      const observer = new MutationObserver((mutations) => {
-        if (document.querySelector(selector)) {
-          observer.disconnect();
-          resolve(document.querySelector(selector));
-        }
-      });
-  
-      observer.observe(document.body, {
-        childList: true,
-        subtree: true,
-      });
-    });
-  }
-  
-  (function () {
-    "use strict";
-    waitForElm(".activity-tab-sort-label").then((elm) => {
-      if (elm.getInnerHTML() != "Oldest first") {
-        elm.click();
+        observer.disconnect();
+        resolve(document.querySelector(selector));
       }
     });
-  })();
-  
+
+    observer.observe(document.body, {
+      childList: true,
+      subtree: true,
+    });
+  });
+}
+
+(function () {
+  "use strict";
+  waitForElm(".activity-tab-sort-label").then((elm) => {
+    if (elm.getInnerHTML() != "Oldest first") {
+      elm.click();
+    }
+  });
+})();
